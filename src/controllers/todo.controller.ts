0b62a632f52afa8f657866e0e93900742d7faaed
davidGBbr <davidgomesbrigido@gmail.com>
2023-10-17 12:14:37 -0300

    Criando uma nova tarefa

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -5,6 +5,17 @@ export const all = async (req: Request, res: Response) => {
   const list = await Todo.findAll();
   res.json({ list });
 };
-export const add = async () => {};
+
+export const add = async (req: Request, res: Response) => {
+  if (req.body.title) {
+    let newTodo = await Todo.create({
+      title: req.body.title,
+      done: req.body.done ? true : false,
+    });
+    res.status(201).json({ item: newTodo });
+  } else {
+    res.json({ error: "Dados não enviados." });
+  }
+};
 export const update = async () => {};
 export const remove = async () => {};
